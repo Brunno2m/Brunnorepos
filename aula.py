e3bb4b4eb5387b4b308d785592e4da3457736c0c
Brunno2m <brunno.m@escolar.ifrn.edu.br>
2024-04-03 22:48:10 -0300

    Adicionando aula.py

---
+++ aula.py
@@ -0,0 +1,10 @@
+val1 = int(input('Insira o primeiro número:'))
+val2 = int(input('Insira o segundo número:'))
+
+soma = val1+val2
+multi = val1*val2
+
+print('O valor da soma é:',soma)
+print('O valor da Multiplicação é:', multi)
+
+
